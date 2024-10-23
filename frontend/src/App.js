f62bee95eca88bdb40ba4163925fcffd295ee532
lyapin <lajpinkonstantin@gmail.com>
2024-10-23 15:44:54 +0300

    add some

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
 import { MapContainer } from 'react-leaflet/MapContainer'
 import { TileLayer } from 'react-leaflet/TileLayer'
+import {Marker, Popup} from "react-leaflet";
 
 function MapShow() {
-    return (<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    return (<MapContainer center={[55.751, 37.617]} zoom={13} scrollWheelZoom={false}>
   <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
+  <Marker position={[55.751, 37.617]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
 </MapContainer>
+
     )
 }
 
