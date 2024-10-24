8f2559f2b975544592dd6deee885b5c3f9d86a2c
lyapin <lajpinkonstantin@gmail.com>
2024-10-24 16:39:11 +0300

    rework map all work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import { MapContainer } from 'react-leaflet/MapContainer'
-import { TileLayer } from 'react-leaflet/TileLayer'
-import {Marker, Popup} from "react-leaflet";
-
-function MapShow() {
-    return (<MapContainer center={[55.751, 37.617]} zoom={13} scrollWheelZoom={false}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[55.751, 37.617]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-</MapContainer>
-
-    )
-}
-
 
 function App() {
-    return (
-        <>
-            <div className="App-map">
-                <MapShow/>
-            </div>
-
-    </>
-
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
