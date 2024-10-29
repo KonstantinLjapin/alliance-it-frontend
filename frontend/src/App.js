027afcd2235a6f31c549b11db6e710334ba91610
lyapin <lajpinkonstantin@gmail.com>
2024-10-29 15:33:29 +0300

    add some

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,38 +2,61 @@ import './App.css';
 import {useState} from "react";
 
 
+function App() {
+    const v = [1, 2, 7];
 
-
-
+    function ApperList(){
+console.log("TAP")
+}
 function ListCoordinates({value}){
     const listItems = value.map((number) => <ol key={number.toString()}>{number}</ol>);
     return(listItems);
 }
 function InputCoordinates() {
-  const [lon, setFirstName] = useState('');
-  function out() { console.log("OK")}
-  return (
-    <>
-        <label>
-            First name:
-            <input
-                value={lon}
-                onChange={e => setFirstName(e.target.value)}
-            />
-            <button onClick={() => out()}>
-                input
-            </button>
-        </label>
-        {lon !== '' &&
-            <p>Your name is {lon}.</p>
+        let [lon, setLon] = useState(null);
+        let [lat, setLat] = useState(null);
+        function SwitcherLATLON(){
+            if (!(!lat !== '')) {
+
+            } else {
+                ApperList();
+            }
+
+
         }
-    </>
+
+  return (
+      <>
+          <label>
+              LON:
+              <input
+                  value={lon}
+                  onChange={e => setLon(e.target.value)}
+              />
+          </label>
+          <label>
+              LAT:
+              <input
+                  value={lat}
+                  onChange={e => setLat(e.target.value)}
+              />
+
+          </label>
+          {lon !== '' &&
+              <p>LON is {lon}.</p>
+          }
+          {lat !== '' &&
+              <p>LAT is {lat}.</p>
+          }
+          <button onClick={() => SwitcherLATLON()}>
+              input
+          </button>
+      </>
   );
+
 }
 
-function App() {
-    const v = [1,2,7]
-    return (
+return (
         <div className="App">
             <div className="wrapper">
                 <div className="block"><ListCoordinates value={v}/></div>
